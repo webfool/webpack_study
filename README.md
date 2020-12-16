ae24e6536ecc24ecbc8dbcc75df23c2697a5416c
Haowen Liu <haowen.liu@shopee.com>
2020-12-16 18:23:41 +0800

    添加 html-webpack-plugin 插件，将打包文件注入 html 中

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 npx webpack 默认将 src/index.js 打包到 dist/main.js
 
-- [ ] 自定义配置文件和 npm script
+- [ ] 自定义 webpack.config.js 配置文件和 npm script
+- [ ] 通过 html-webpack-plugin 插件，将打包文件注入 html 中
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'), // path 必须是绝对路径
     filename: 'main.[hash:8].js'
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html', // html 模板路径
+      filename: 'index.html', // html 输出文件名
+      // hash: true, // 在静态资源路径后面加 hash 值，如 a.js?abcdef
+      // publicPath: '/cdn/path' // 静态资源路径前缀
+      minify: {
+        collapseWhitespace: true, // 打包成一行
+      }
+    })
+  ]
 }
