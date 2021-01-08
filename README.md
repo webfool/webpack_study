29b96de7d120120ac63f732295d1e88e9d76ca99
Haowen Liu <haowen.liu@shopee.com>
2021-01-08 15:41:47 +0800

    配置gzip压缩

--- README.md
+++ README.md
@@ -390,3 +390,24 @@ setTimeout(() => {
   })
 }, 3000)
 ```
+
+- 开始gzip压缩
+```js
+npm i -D compression-webpack-plugin
+// webpack.prod.js
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+
+module.exports = {
+  plugins: [
+    new CompressionWebpackPlugin({ // 开启 gzip 压缩
+      test: new RegExp('\\.(js|css)$'),
+      exclude: /doc\/*/,
+      threshold: 1, // 压缩大于 1b 的文件
+      algorithm: 'gzip',
+      minRatio: 0.8, // 当 压缩后/压缩前 的比 <= 0.8 时，就会触发压缩
+      filename: '[path][base].gz',
+      deleteOriginalAssets: false // 压缩之后不删除源文件
+    })
+  ]
+}
+```
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,18 @@
 const { merge } = require('webpack-merge')
 const base = require('./webpack.base')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
 
 module.exports = merge(base, {
-  mode: 'production'
+  mode: 'production',
+  plugins: [
+    new CompressionWebpackPlugin({ // 开启 gzip 压缩
+      test: new RegExp('\\.(js|css)$'),
+      exclude: /doc\/*/,
+      threshold: 1, // 压缩大于 1b 的文件
+      algorithm: 'gzip',
+      minRatio: 0.8, // 当 压缩后/压缩前 的比 <= 0.8 时，就会触发压缩
+      filename: '[path][base].gz',
+      deleteOriginalAssets: false // 压缩之后不删除源文件
+    })
+  ]
 })
